1e4234c28e3bf228d6de844d29a0207b1d90bc95
ramchandraallu@gmail.com <ramchandraallu@gmail.com>
2021-12-29 15:03:49 +0530

    intial commit

---
+++ test/src/test/java/maventest/JarTest.java
@@ -0,0 +1,10 @@
+package maventest;
+
+public class JarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a jar test ");
+	}
+
+}
